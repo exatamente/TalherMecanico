0c6117f79148f3982bd87d5c5d9473ac0ae6383a
ibrahimorra <55318588+ibrahimorra@users.noreply.github.com>
2019-10-06 15:57:58 -0300

    Add files via upload

---
+++ stylesheets/homeStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    inputSection: {
+        paddingTop: 35,
+        alignItems: 'center'
+    },
+
+    Button: {
+        margin: 55,
+        paddingTop: 25,
+        marginTop: 0
+    }
+
+});
